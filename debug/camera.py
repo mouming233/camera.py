8191114ec76b57c59ad6ae43d3f691b0d4bf318d
mouming <102670223+mouming233@users.noreply.github.com>
2023-03-29 16:12:07 +0800

    Update camera.py

--- debug/camera.py
+++ debug/camera.py
@@ -1,59 +1,44 @@
 import cv2
-import numpy as np
+import rospy
 import socket
-import struct
-import threading
+import rospy
+from sensor_msgs.msg import Image, CameraInfo
+from cv_bridge import CvBridge
+from image_geometry import PinholeCameraModel
+from geometry_msgs.msg import PointStamped
+import numpy as np
 
+subscribed=False
+bridge = CvBridge()
 
-
-# 创建TCP/IP套接字
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
-#绑定(主机,端口号)到套接字
-s.bind(('10.236.112.215', 8888))
-
-
-#开始TCP监听
-s.listen(1)
-
-# 建立客户端连接,阻塞
-# 等待客户端连接
-print("等待客户端连接...")
-conn, addr = s.accept()
-print("已连接：", addr)
-
-
-while True:
-    # 接收TCP数据
-    data=conn.recv(4)
-
-    if not data:
-        break
-
-    img_size = struct.unpack('!i', data)[0]
-
-    img_data=b''
-
-    while len(img_data)<img_size:
-
-        data=conn.recv(img_size-len(img_data))
-
-        if not data:
-            break
-
-        img_data+=data
-
-
-    img = cv2.imdecode(np.frombuffer(img_data, np.uint8), cv2.IMREAD_COLOR)
-
-    cv2.imshow("img",img)
-
-    cv2.waitKey(0)
+s.connect(('10.241.8.183', 8888))
 
 
 
+def depth_image_callback(msg):
 
-conn.close()
+    #将图像转换到opencv下（passthrough保留原始编码格式）
+    global depth_image,subscribed
+    subscribed=True
+    print(subscribed)
+    depth_image = bridge.imgmsg_to_cv2(msg,desired_encoding='passthrough')
+    #获取相机坐标系下的坐标
+
+    img_data = cv2.imencode('.jpg', depth_image)[1].tobytes()
+    s.sendall(len(img_data).to_bytes(4, byteorder='big'))
+    s.sendall(img_data)
+
+
+rospy.init_node("target_site")
+
+
+rospy.Subscriber("/qingzhou/camera_link/image_raw",Image,depth_image_callback)
+
+
+response = s.recv(1024)
+print(response.decode())
+
+
 s.close()
-cv2.destroyAllWindows()
 
